afec96793b28005796140e640bf22b87b2827cea
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-06-21 17:11:54 +0300

    Implement top-level lexing rules (partially).

--- src/driver.rs
+++ src/driver.rs
@@ -1,40 +1,70 @@
 use crate::*;
 use crate::lexeme::Lexeme;
 
-/// Generates Haskell token stream, uses a string view on the source
+/// Generates Haskell token stream, uses a string view on the source code.
+/// Operates on the top-level lexemes. These '__lexemes__' are handled at
+/// other levels.
+/// ---- Rules covered at this scope ----
+/// `program -> whitespace | lexeme`
+///
+/// `whitespace -> whitestuff {whitestuff}
+/// `whitestuff -> whitechar | __comment__ | __mlcomment__
+/// `whitechar -> '\n' | '\r' | ' ' | '\t'
+///
+/// `lexeme -> __qvarid__ | __qconid__ | __qvarsym__ | __qconsym__
+///          | __literal__ | __special__ | __reservedop__ | __reservedid__
 
-fn gen_hs_token_stream(string_view : &str) {
-    // the value returned
+fn gen_hs_token_stream(string_view : &str) -> Vec<token::Token>{
+    // return value
     let mut token_stream : Vec<token::Token> = vec![];
     // how many characters away from the beginning of file
-    let mut advanced_chars : usize = 0;
+    let mut buffer_offset : usize = 0;
 
     // current position in the source buffer
     let mut cur_row : u32 = 0;
     let mut cur_col : u16 = 0;
 
-    while advanced_chars < string_view.len() {
+    // only handle basic whitestuff, the 2010 report names more characters (vertical
+    // tab, form feed, any unicode char that represents whitespace)
+    let whitechar = vec!['\n', '\r', '\t', ' '];
 
-        // for every lexeme, try to recognize it
-        // problem : some lexemes depend on the order of recognition
+    let lexemes = vec![mlcomment::MLComment::recognize];
 
-        if let Some(token) = mlcomment::MLComment::recognize(
-            &string_view[advanced_chars..]) {
-            advanced_chars += token.span.iter()
-                .fold(token.span.len() - 1, |sum, x| sum + *x as usize);
+    while buffer_offset < string_view.len() {
 
-            cur_row += token.span.len() as u32;
-            cur_col = *token.span.last().unwrap();
-            token_stream.push(token);
+        // whitespace-like (non-comment)
+        if whitechar.contains(&string_view.chars().nth(buffer_offset).unwrap()) {
+            buffer_offset += 1;
+            continue
+        }
+
+        for recognizer in &lexemes {
+            if let Some(token) = recognizer(&string_view[buffer_offset..]) {
+                // todo no cast?
+                buffer_offset += token.span.iter()
+                    .fold(token.span.len() - 1, |sum, x| sum + *x as usize);
+                cur_row += token.span.len() as u32;
+                cur_col = *token.span.last().unwrap();
+                token_stream.push(token)
+            } else {
+                println!("No token recognized")
+            }
         }
     }
+
+    token_stream
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn run() {
-        gen_hs_token_stream("dfsfdf");
+    fn mlcomment() {
+        assert_eq!(gen_hs_token_stream("{--}"), vec![
+            token::Token
+            {
+                span : vec![4],
+                token_type : token::TokenType::MLComment
+            }]);
     }
 }
